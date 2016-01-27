81b5a9b1344786780bc5d0d26b12e04e2ba953d7
nathaniel smith <nathanielksmith@gmail.com>
2016-01-26 20:23:21 -0800

    everybody dance

--- booth.sh
+++ booth.sh
@@ -2,9 +2,11 @@
 
 action=$1
 
-upload_path="vilmibm@tilde.town:/home/public_html/asciigallery/portraits/"
+local_portrait_path="portraits"
+upload_path="vilmibm@tilde.town:/home/vilmibm/public_html/asciigallery"
+remote_portrait_path="$upload_path/portraits/"
 screenshot=00000001.png
-final=$(date --iso-8601=seconds -u).portrait
+final="$local_portrait_path/$(date --iso-8601=seconds -u).portrait"
 
 function print () {
     echo "printing..." >&2
@@ -13,29 +15,43 @@ function print () {
 
 function upload () {
     echo "uploading..." >&2
-    jp2a --height=60 --width=100 --output=$final.html --html --colors --fill shotsmall.jpg
-    scp $final.html $upload_path
+    jp2a --height=30 --width=60 --output=$final.html --html --colors --fill /tmp/shotsmall.jpg
+
+    scp $final.html $remote_portrait_path
+
+    if [ -z $(ls index.html 2>/dev/null) ]; then
+        cp index.html index.html.bak
+    fi
+
+    cp index.top.html index.html
+
+    for portrait in $(ls $local_portrait_path | egrep "html$"); do
+        echo "<div class=\"portrait\">" >> index.html
+        # TODO var for url
+        echo "<iframe src=\"portraits/$portrait\"></iframe>" >> index.html
+        echo "</div>" >> index.html
+    done
+
+    echo "</div></body></html>" >> index.html
+    scp index.html $upload_path/
 }
 
 mplayer -vo png -frames 1 tv:// > /dev/null 2>/dev/null
-convert $screenshot -gravity Center -crop 200x200+0+0 +repage shotcrop.jpg > /dev/null
-convert shotcrop.jpg -scale 48x48 shotsmall.jpg > /dev/null
-jp2a --height=60 --width=80 --output=$final.txt --invert shotsmall.jpg
+convert $screenshot -gravity Center -crop 200x200+0+0 +repage /tmp/shotcrop.jpg > /dev/null
+convert /tmp/shotcrop.jpg -scale 48x48 /tmp/shotsmall.jpg > /dev/null
+jp2a --height=60 --width=80 --output=$final.txt --invert /tmp/shotsmall.jpg
 
 cat $final.txt
 
-if [ "$action" == "print" ];
-then
+if [ "$action" == "print" ]; then
     print
-elif [ "$action" == "upload" ];
-then
+elif [ "$action" == "upload" ]; then
     upload
-elif [ "$action" == "all" ];
-then
+elif [ "$action" == "all" ]; then
     print
     upload
 fi
 
 rm $screenshot
-rm shotcrop.jpg
-rm shotsmall.jpg
+rm /tmp/shotcrop.jpg
+rm /tmp/shotsmall.jpg
